e94204c6253638531e261f4ec8ae0dd9b4630705
egocentryk <lukasz.skowron@gmail.com>
2025-02-27 11:20:03 +0100

    feat(): decorating model properties

--- src/main.ts
+++ src/main.ts
@@ -34,6 +34,7 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, options)
   SwaggerModule.setup('api', app, document)
+
   await app.listen(process.env.PORT ?? 3001)
 }
 bootstrap()
