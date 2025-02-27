c96706b88e194496147c9288d9c6d1537c9140cc
egocentryk <lukasz.skowron@gmail.com>
2025-02-27 11:51:13 +0100

    feat(): getting started with test suites

--- src/coffees/coffees.service.spec.ts
+++ src/coffees/coffees.service.spec.ts
@@ -1,18 +1,29 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { CoffeesService } from './coffees.service';
+import { Test, TestingModule } from '@nestjs/testing'
+import { CoffeesService } from './coffees.service'
+import { DataSource } from 'typeorm'
+import { getRepositoryToken } from '@nestjs/typeorm'
+import { Coffee } from './entities/coffee.entity'
+import { Flavor } from './entities/flavor.entity'
+import { getConfigToken } from '@nestjs/config'
 
 describe('CoffeesService', () => {
-  let service: CoffeesService;
+  let service: CoffeesService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CoffeesService],
-    }).compile();
+      providers: [
+        CoffeesService,
+        { provide: DataSource, useValue: {} },
+        { provide: getRepositoryToken(Coffee), useValue: {} },
+        { provide: getRepositoryToken(Flavor), useValue: {} },
+        { provide: getConfigToken('coffees'), useValue: {} },
+      ],
+    }).compile()
 
-    service = module.get<CoffeesService>(CoffeesService);
-  });
+    service = module.get<CoffeesService>(CoffeesService)
+  })
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+    expect(service).toBeDefined()
+  })
+})
